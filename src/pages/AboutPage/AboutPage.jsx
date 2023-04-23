e42b8b0fefbfd2903c778448603a0ef5dbcd1510
Gurleen Singh <gs585@drexel.edu>
2023-04-23 13:11:36 -0400

    Turned AboutPage into class component

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -5,18 +5,36 @@ import OverlayContainer from "terra-overlay/lib/OverlayContainer";
 import Button from "terra-button";
 import Grid from "terra-grid";
 
-const AboutPage = () => (
-  <div>
-    <ActionHeader title="This is the about page." />
-    <Grid.Row>
-      <Grid.Column tiny={6}>
-        <Link to="/">Home Page</Link>
-      </Grid.Column>
-      <Grid.Column tiny={6}>
-        <Button onClick={() => alert("Hello!")} text="Show alert" />
-      </Grid.Column>
-    </Grid.Row>
-  </div>
-);
+class AboutPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  onButtonClick() {
+    this.setState({
+      count: this.state.count += 1
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <ActionHeader title="This is the about page." />
+        <Grid.Row>
+          <Grid.Column tiny={6}>
+            <Link to="/">Home Page</Link>
+          </Grid.Column>
+          <Grid.Column tiny={6}>
+            <Button onClick={() => this.onButtonClick()} text="Add one" />
+            <p>Count: {this.state.count}</p>
+          </Grid.Column>
+        </Grid.Row>
+      </div>
+    );
+  }
+}
 
 export default AboutPage;
