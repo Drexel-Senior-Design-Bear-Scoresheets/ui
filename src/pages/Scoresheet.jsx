310293641c4d333807c86209e3c86b11d021c8f8
rafayahmed40 <ra694@drexel.edu>
2023-05-24 11:51:47 -0400

    added scoresheet functionality

--- src/pages/Scoresheet.jsx
+++ src/pages/Scoresheet.jsx
@@ -1,55 +1,64 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/scoresheet.css';
 import Button from 'terra-button';
 import Radio from 'terra-form-radio';
 import Input from 'terra-form-input';
 import Text from 'terra-text';
 
+import Toolbar from 'terra-toolbar';
+import IconProvider from 'terra-icon/lib/icon/IconProvider';
+import IconHouse from 'terra-icon/lib/icon/IconHouse';
+
+
+
+
 const ScoreSheetCreator = () => {
   const initialQuestions = [
-    { id: 1, text: 'Insert Scoresheet Title' },
-    { id: 2, text: 'Insert Scoresheet Information' },
-    { id: 3, text: 'Insert Reference Information' },
+    { id: 1, text: 'Insert Scoresheet Title', data: '' },
+    { id: 2, text: 'Insert Scoresheet Information', data: '' },
+    { id: 3, text: 'Insert Reference Information', data: '' },
   ];
 
   const [questions, setQuestions] = useState(initialQuestions);
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [additionalQuestions, setAdditionalQuestions] = useState([
-    { id: 1, text: 'Insert Question 1' , answerType: 'Yes/No'},
+    { id: 1, text: 'Insert Question 1', answerType: 'Yes/No', data: '', showInputField: false, inputFields: [] },
   ]);
 
-  const handleNextClick = () => {
-    if (currentQuestionIndex < questions.length - 1) {
-      setCurrentQuestionIndex(currentQuestionIndex + 1);
-    } else if (currentQuestionIndex === questions.length - 1) {
-      setCurrentQuestionIndex(questions.length);
-    }
-  };
-
-  const handleBackClick = () => {
-    if (currentQuestionIndex > 0 && currentQuestionIndex <= questions.length) {
-      setCurrentQuestionIndex(currentQuestionIndex - 1);
-    } else if (currentQuestionIndex > questions.length) {
-      setCurrentQuestionIndex(questions.length);
-    }
-  };
-
-  const handleQuestionInputChange = (event) => {
-    const updatedQuestions = [...questions, ...additionalQuestions];
-    updatedQuestions[currentQuestionIndex].text = event.target.value;
+  const handleQuestionInputChange = (event, index) => {
+    const updatedQuestions = [...questions];
+    updatedQuestions[index].data = event.target.value;
     setQuestions(updatedQuestions);
   };
 
   const handleAnswerTypeInputChange = (event, index) => {
-    const updatedQuestions = [...questions, ...additionalQuestions];
+    const updatedQuestions = [...additionalQuestions];
     updatedQuestions[index].answerType = event.target.value;
-    setAdditionalQuestions(updatedQuestions.slice(questions.length));
+    updatedQuestions[index].showInputField = event.target.value === 'Multiple Option';
+    setAdditionalQuestions(updatedQuestions);
   };
 
   const handleAdditionalQuestionInputChange = (event, index) => {
-    const updatedQuestions = [...questions, ...additionalQuestions];
-    updatedQuestions[index].text = event.target.value;
-    setAdditionalQuestions(updatedQuestions.slice(questions.length));
+    const updatedQuestions = [...additionalQuestions];
+    updatedQuestions[index].data = event.target.value;
+    setAdditionalQuestions(updatedQuestions);
+  };
+
+  const handleInputFieldChange = (event, questionIndex, inputIndex) => {
+    const updatedQuestions = [...additionalQuestions];
+    updatedQuestions[questionIndex].inputFields[inputIndex] = event.target.value;
+    setAdditionalQuestions(updatedQuestions);
+  };
+
+  const handleAddInputField = (index) => {
+    const updatedQuestions = [...additionalQuestions];
+    updatedQuestions[index].inputFields.push('');
+    setAdditionalQuestions(updatedQuestions);
+  };
+
+  const handleRemoveInputField = (index, inputIndex) => {
+    const updatedQuestions = [...additionalQuestions];
+    updatedQuestions[index].inputFields.splice(inputIndex, 1);
+    setAdditionalQuestions(updatedQuestions);
   };
 
   const handleRemoveQuestionClick = (index) => {
@@ -57,8 +66,8 @@ const ScoreSheetCreator = () => {
       const updatedQuestions = [...prevQuestions];
       updatedQuestions.splice(index, 1);
       return updatedQuestions.map((question, i) => {
-        question.id = i;
-        question.text = `Question ${i + 2}`;
+        question.id = i + 1;
+        question.text = `Insert Question ${i + 2}`;
         question.answerType = 'Yes/No';
         return question;
       });
@@ -70,51 +79,173 @@ const ScoreSheetCreator = () => {
       id: additionalQuestions.length + 1,
       text: 'Insert Question ' + (additionalQuestions.length + 1),
       answerType: 'Yes/No',
+      showInputField: false,
+      inputFields: [],
     };
     setAdditionalQuestions([...additionalQuestions, newQuestion]);
   };
 
-  const handleCreateScoresheet = () => {
-    // Handle creating the scoresheet based on the questions
-    // This function will depend on your specific implementation
+  const [selectedKeyword, setSelectedKeyword] = useState('');
+  
+  const keywords = ["EDF", "ADHD DHA-5"];
+
+  const handleKeywordChange = (event) => {
+    setSelectedKeyword(event.target.value);
   };
 
+  const handleCreateScoresheet = () => {
+    const scoresheetData = {
+      scoresheetTitle: questions[0].data,
+      scoresheetInformation: questions[1].data,
+      scoresheetReference: questions[2].data,
+      selectKeyword: selectedKeyword,
+      additionalQuestions: additionalQuestions.map((question) => ({
+        text: question.data,
+        answerType: question.answerType,
+        inputFields: question.inputFields,
+      })),
+    };
+    console.log(scoresheetData);
+  }
+    /*
+  
+    // Send the scoresheetData object in a POST API call
+    fetch('your-api-endpoint', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(scoresheetData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the response data if needed
+        console.log(data);
+      })
+      .catch((error) => {
+        // Handle the error if needed
+        console.error(error);
+      });
+  };
+  */
+
+
   return (
-    <div>
-      {currentQuestionIndex < questions.length ? (
+    <center>
+      <Toolbar ariaLabel="NavBar" ariaControls="content" className='Toolbar'>
+        <Button text="Edit" variant="utility" icon={<IconHouse className='Homeicon' />} className='Home' height='24em' width='24em' />
+        <Button text="Edit" variant="utility" icon={<IconProvider className='Providericon' />} className='Provider' height='24em' width='24em' />
+      </Toolbar>
+      <div className="scoresheet-form">
+        <h1>Create New Scoresheet</h1>
         <div className="fade-in">
-          <Text fontSize={20}>{questions[currentQuestionIndex].text}</Text>
-          <input type="text" value={questions[currentQuestionIndex].text} onChange={handleQuestionInputChange} />
-          <Button text="Back" onClick={handleBackClick} ></Button>
-          <Button text="Next" onClick={handleNextClick} ></Button>
-        </div>
-      ) : (
-        <div>
-          {additionalQuestions.map((question, index) => (
-            <div key={question.id} className="fade-in">
+          {questions.map((question, index) => (
+            <div key={question.id}>
               <Text fontSize={20}>{question.text}</Text>
-              <Input name="default blank input" id="blank" ariaLabel={question.text} onChange={(event) => handleAdditionalQuestionInputChange(event, index)}/>
-              <div>
-                <Radio id="first-inline" labelText="Yes/No" name={`inline-example-${index}`} isInline checked={question.answerType === 'Yes/No'} onChange={(event) => handleAnswerTypeInputChange(event, index)}/>
-                <Radio id="second-inline" labelText="Multiple Option" name={`inline-example-${index}`} checked={question.answerType === 'Yes/No'} onChange={(event) => handleAnswerTypeInputChange(event, index)}isInline />
-                <Radio id="third-inline" labelText="Scalar" name={`inline-example-${index}`} isInline checked={question.answerType === 'Yes/No'} onChange={(event) => handleAnswerTypeInputChange(event, index)}/>
-                </div>
-                <Button text="Remove Question" onClick={() => handleRemoveQuestionClick(index)} ></Button>
+              <div className="text-input">
+                <Input
+                  name={`question-${index}`}
+                  id={`question-${index}`}
+                  onChange={(event) => handleQuestionInputChange(event, index)}
+                />
+              </div>
             </div>
           ))}
-          <div className="fade-in">
-            <Button text="Back" onClick={handleBackClick} ></Button>
-            <Button text="Add Question" onClick={handleAddQuestion} ></Button>
-            <Button text="Next" onClick={handleNextClick} ></Button>
-          </div>
         </div>
-      )}
-      <div className="fade-in">
-        {currentQuestionIndex === questions.length + additionalQuestions.length && (
-          <Button text="Create Scoresheet" onClick={handleCreateScoresheet} ></Button>
-        )}
-      </div>
+        <div>
+      {keywords.map((keyword) => (
+        <label key={keyword}>
+          <input
+            type="radio"
+            value={keyword}
+            checked={selectedKeyword === keyword}
+            onChange={handleKeywordChange}
+          />
+          {keyword}
+        </label>
+      ))}
     </div>
+        
+        {additionalQuestions.map((question, index) => (
+          <div key={question.id} className="fade-in">
+            <Text fontSize={20}>{question.text}</Text>
+            <div className="text-input">
+              <Input
+                name={`additional-question-${index}`}
+                id={`additional-question-${index}`}
+                onChange={(event) => handleAdditionalQuestionInputChange(event, index)}
+              />
+            </div>
+            <div>
+              <label>Yes/No</label>
+              <Radio
+                id={`answer-type-yesno-${index}`}
+                name={`answer-type-${index}`}
+                checked={question.answerType === 'Yes/No'}
+                value="Yes/No"
+                onChange={(event) => handleAnswerTypeInputChange(event, index)}
+                isInline
+              />
+              <label>Multiple Option</label>
+              <Radio
+                id={`answer-type-multiple-${index}`}
+                name={`answer-type-${index}`}
+                checked={question.answerType === 'Multiple Option'}
+                value="Multiple Option"
+                onChange={(event) => handleAnswerTypeInputChange(event, index)}
+                isInline
+              />
+              <label>Scalar</label>
+              <Radio
+                id={`answer-type-scalar-${index}`}
+                name={`answer-type-${index}`}
+                checked={question.answerType === 'Scalar'}
+                value="Scalar"
+                onChange={(event) => handleAnswerTypeInputChange(event, index)}
+                isInline
+              />
+              {question.showInputField && (
+                <div>
+                  <Text fontSize={20}>Input Fields:</Text>
+                  {question.inputFields.map((input, inputIndex) => (
+                    <div key={inputIndex}>
+                      <div className="text-input">
+                        <Input
+                          name={`input-field-${index}-${inputIndex}`}
+                          id={`input-field-${index}-${inputIndex}`}
+                          onChange={(event) => handleInputFieldChange(event, index, inputIndex)}
+                        />
+                      </div>
+                      <Button
+                        className="button"
+                        text="Remove Input Field"
+                        onClick={() => handleRemoveInputField(index, inputIndex)}
+                      />
+                    </div>
+                  ))}
+                  <Button
+                    className="button"
+                    text="Add Input Field"
+                    onClick={() => handleAddInputField(index)}
+                  />
+                </div>
+              )}
+              <Button
+                className="button"
+                text="Remove Question"
+                onClick={() => handleRemoveQuestionClick(index)}
+              />
+            </div>
+          </div>
+        ))}
+        <div className="fade-in">
+          <Button className="button" text="Add Question" onClick={handleAddQuestion}></Button>
+        </div>
+        <div>
+          <Button className="button" text="Create Scoresheet" onClick={handleCreateScoresheet}></Button>
+        </div>
+      </div>
+    </center>
   );
 };
 
