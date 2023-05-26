2935084a3ae87cd75c98a12410fe20cc511732b7
rafayahmed40 <ra694@drexel.edu>
2023-05-25 22:19:40 -0400

    Added styling

--- src/pages/Fillout.jsx
+++ src/pages/Fillout.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import '../styles/scoresheet.css';
 
 const Fillout = () => {
   const [scoreSheetData, setScoreSheetData] = useState(null);
